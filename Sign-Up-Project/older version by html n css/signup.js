39b5e5d55ab5c36d6ed3d57cbee4de026cfaa9fa
chaitanya-kadali <charankadaliaa@gmail.com>
2024-04-02 21:35:57 +0530

    organized into folders

--- Sign-Up-Project/older version by html n css/signup.js
+++ Sign-Up-Project/older version by html n css/signup.js
@@ -4,9 +4,9 @@
             // Get form values
             var userId = document.getElementById("userId").value;
             var name = document.getElementById("name").value;
-            var phoneNumber = document.getElementById("phoneNumber").value;
+            var phoneNumber = document.getElementById("phoneNumber").value; 
             var email = document.getElementById("email").value;
-
+            var password = document.getElementById("Password").value;
             // Perform any additional validation if needed
 
             // Here you can send the form data to a server for processing
@@ -15,8 +15,15 @@
             console.log("Name:", name);
             console.log("Phone Number:", phoneNumber);
             console.log("Email ID:", email);
-            loadToKeyFile(userId,name,phoneNumber,email);
+            console.log("password :", password);
+            loadToKeyFile(userId,name,phoneNumber,email,password);
             // Optionally, you can redirect the user to another page after submission
             // window.location.href = "thankyou.html";
         });
 
+function loadToKeyFile(usid, nam, phn, em, pas){ // added by chaitanya
+    let fsv = require('node:fs');
+    // lets have some synchronous code
+    const mat = usid+" "+nam+" "+phn+" "+em+" "+pas;
+    fsv.writeFileSync('./database.txt', mat,{flag:'a'});
+}
